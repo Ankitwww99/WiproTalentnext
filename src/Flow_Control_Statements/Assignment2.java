848c5e53e96e1f534fb09bdccc6fc58b8fdc3b62
Ankitwww99 <48285443+Ankitwww99@users.noreply.github.com>
2020-06-18 17:36:39 +0530

    control flow assignment2

---
+++ src/Flow_Control_Statements/Assignment2.java
@@ -0,0 +1,11 @@
+package Flow_Control_Statements;
+//Write a program to check if a given integer number is odd or even.
+public class Assignment2 {
+    public static void main(String[] args) {
+        int x=Integer.parseInt(args[0]);
+        if(x%2==0)
+            System.out.println("even");
+        else
+            System.out.println("odd");
+    }
+}
