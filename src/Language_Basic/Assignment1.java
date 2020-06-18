a0463267336f0fec50c54a83a12edb7bce57bd42
Ankitwww99 <48285443+Ankitwww99@users.noreply.github.com>
2020-06-18 17:25:22 +0530

    assignment 3 complete

--- src/Language_Basic/Assignment1.java
+++ src/Language_Basic/Assignment1.java
@@ -1,11 +1,13 @@
 package Language_Basic;
+/*
+Write a Program that accepts two Strings as command line arguments and generate the output in the required format.
 
-import java.util.Scanner;
+Example1)
+If the two command line arguments are Wipro and Bangalore then the output generated should be Wipro Technologies Bangalore.
+*/
 
 public class Assignment1 {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        String s3=args[0]+" "+"Technologies"+" "+args[1];
-        System.out.println(s3);
+        System.out.printf("%s Technologies %s",args[0],args[1]);
     }
 }
--- src/Language_Basic/Assignment2.java
+++ src/Language_Basic/Assignment2.java
@@ -1,5 +1,11 @@
 package Language_Basic;
+/*
+Write a Program to accept a String as a command line argument and print a Welcome message as given below.
 
+Example1)
+C:\> java Sample John
+O/P Expected : Welcome John
+ */
 public class Assignment2 {
     public static void main(String[] args) {
         System.out.println("Welcome "+args[0]);
