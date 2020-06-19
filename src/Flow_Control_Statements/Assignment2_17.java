608fb8ddd03f98ccca74fa00861073be9ee580c4
Ankitwww99 <48285443+Ankitwww99@users.noreply.github.com>
2020-06-19 16:02:41 +0530

    assignment2_17 complete

--- src/Flow_Control_Statements/Assignment2_17.java
+++ src/Flow_Control_Statements/Assignment2_17.java
@@ -1,5 +1,15 @@
 package Flow_Control_Statements;
+/*
+ Write a Java program to find if the given number is palindrome or not
 
+Example1)
+C:\>java Sample 110011
+O/P: 110011 is a palindrome
+
+Example2)
+C:\>java Sample 1234
+O/P: 1234 is not a palindrome
+ */
 public class Assignment2_17 {
     public static boolean isPrime(int n){
         for(int i=2;i<=n/2;i++){
