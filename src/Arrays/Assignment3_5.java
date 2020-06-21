ed343e26bbbf5e30996bc27b12caa903be5942dd
Ankitwww99 <48285443+Ankitwww99@users.noreply.github.com>
2020-06-21 13:54:54 +0530

    assignment3_5 complete

--- src/Arrays/Assignment3_5.java
+++ src/Arrays/Assignment3_5.java
@@ -2,15 +2,11 @@ package Arrays;
 /*
 Write a program to find the largest 2 numbers and the smallest 2 numbers in the given array.
  */
-import java.util.Arrays;
 
 public class Assignment3_5 {
     public static void main(String[] args) {
         int[] ar={3,6,1,92,34,47,66,77,96};
-        /* alternate way
-        Arrays.sort(ar);
-        System.out.printf("Max1=%d and Min1=%d \nMax2=%d and Min2=%d",ar[ar.length-1],ar[0],ar[ar.length-2],ar[1]);
-        */
+
         int max1=ar[0],min1=ar[0];
         int max2=ar[0],min2=ar[0];
         for(int i=1;i<ar.length;i++){
