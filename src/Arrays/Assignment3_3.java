6780f122b596e1cb6c144fc0b315cb4f92cae052
Ankitwww99 <48285443+Ankitwww99@users.noreply.github.com>
2020-06-21 13:52:29 +0530

    assignment3_3 complete

--- src/Arrays/Assignment3_3.java
+++ src/Arrays/Assignment3_3.java
@@ -1,5 +1,13 @@
 package Arrays;
+/*
+Write a program to initialize an integer array with values and check if a given number is present in the array or not.
 
+If the number is not found, it will print -1 else it will print the index value of the given  number in the array.
+
+Example 1) If the Array elements are  {1,4,34,56,7} and the search element is 90, then the output expected is -1.
+
+Example 2)If the Array elements are  {1,4,34,56,7} and the search element is 56, then the output expected is 3.
+ */
 import java.util.Scanner;
 
 public class Assignment3_3 {
