1d0285387f511b62a17a059726328d2a2d5709c3
Ankitwww99 <48285443+Ankitwww99@users.noreply.github.com>
2020-06-21 10:39:27 +0530

    assignment3_4 complete

---
+++ src/Arrays/Assignment3_4.java
@@ -0,0 +1,10 @@
+package Arrays;
+
+public class Assignment3_4 {
+    public static void main(String[] args) {
+        int[] a={45,46,97,56,112,100,78,48,47,55};
+        for(int i=0;i<a.length;i++){
+            System.out.print((char)a[i]+" ");
+        }
+    }
+}
