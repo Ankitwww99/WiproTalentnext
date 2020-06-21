93f5c1944094757ae68f5f3c9362e6296a40a61e
Ankitwww99 <48285443+Ankitwww99@users.noreply.github.com>
2020-06-21 14:02:31 +0530

    assignment3_12 question update

--- src/Arrays/Assignment3_12.java
+++ src/Arrays/Assignment3_12.java
@@ -1,5 +1,11 @@
 package Arrays;
+/*
+Given 2 int arrays, a and b, each length 3, form a new array of length 2, containing their middle elements.
 
+middleWay([1, 2, 3], [4, 5, 6]) → [2, 5]
+middleWay([7, 7, 7], [3, 8, 0]) → [7, 8]
+middleWay([5, 2, 9], [1, 4, 5]) → [2, 4]
+ */
 import java.util.Scanner;
 
 public class Assignment3_12 {
