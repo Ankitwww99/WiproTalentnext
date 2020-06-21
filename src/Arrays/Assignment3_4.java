045cf91f036b6da1c7c9615173c4a943f0277228
Ankitwww99 <48285443+Ankitwww99@users.noreply.github.com>
2020-06-21 13:53:08 +0530

    assignment3_4 complete

--- src/Arrays/Assignment3_4.java
+++ src/Arrays/Assignment3_4.java
@@ -1,5 +1,7 @@
 package Arrays;
-
+/*
+ Initialize an integer array with ascii values and print the corresponding character values in a single row.
+ */
 public class Assignment3_4 {
     public static void main(String[] args) {
         int[] a={45,46,97,56,112,100,78,48,47,55};
